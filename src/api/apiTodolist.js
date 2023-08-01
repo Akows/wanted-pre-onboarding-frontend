bc94bc68a7c7cc88e71857c7816849e49744cf8c
AKOWSPC <akows141@gmail.com>
2023-08-01 16:00:19 +0900

    [프로젝트 배포] [프로젝트 유지보수] - 로그인 이후 todo 페이지로 진입하여 데이터를 조회해 올때 간헐적으로 401 에러가 발생되는 문제를 해결했으나, 이후로도 문제가 재발하여 다른 해결책을 적용함. - 문제의 원인은 웹 스토리지의 토큰이 저장되기 전에 todo 데이터를 가져오는 메소드가 먼저 동작하는 것. - 따라서 axios 요청이 실행되기 이전에 Axios config와 token 등이 정상적으로 가져와지도록 코드 구조를 수정함. - 수정 이후 문제가 사라졌음을 확인함.
    - 또한 로컬 배포와 달리 vercel 배포 환경에서 axios 요청 URL 문제로 기능이 정상 동작하지 않는 문제를 발견함.
    - 따라서 1번 문제를 해결하는 과정에서 baseURL 속성을 설정하고, 환경변수와 같이 적용하여 요청 URL 문제를 해결함.
    - 수정 이후 문제가 사라졌음을 확인함.

--- src/api/apiTodolist.js
+++ src/api/apiTodolist.js
@@ -1,33 +1,61 @@
-import axios from 'axios';
-
-const token = localStorage.getItem('token');
-
-const header = {
-    headers: {
-        // eslint-disable-next-line
-        Authorization: 'Bearer ' + `${token}`,
-        'Content-Type': 'application/json',
-    },
-};
+import { apiClient } from './apiClient';
 
 // todo 작성.
 const addTodo = async (payload) => {
-    return await axios.post(`${process.env.REACT_APP_API_URL}todos`, payload, header);
+    return await apiClient.post('/todos', payload);
 };
 
 // todo 조회.
 const getTodo = async () => {
-    return await axios.get(`${process.env.REACT_APP_API_URL}todos`, header);
+    return await apiClient.get('todos');
 };
 
 // todo 수정.
 const updateTodo = async (id, payload) => {
-    return await axios.put(`${process.env.REACT_APP_API_URL}todos/${id}`, payload, header);
+    return await apiClient.put(`todos/${id}`, payload);
 };
 
 // todo 삭제.
 const deleteTodo = async (id) => {
-    return await axios.delete(`${process.env.REACT_APP_API_URL}todos/${id}`, header);
+    return await apiClient.delete(`todos/${id}`);
 };
+
+// 이전 버전 코드.
+// 참고용도로 삭제하지는 않고 전체 주석처리.
+// 이전 버전의 경우 토큰이 웹 스토리지에 존재함에도, 이를 제대로 가져오기 전에 서버에 요청을 먼저 보내어..
+// 토큰이 없는 상황에서 서버에 인증을 요청하여 지속적으로 문제를 발생시켰음.
+
+// import axios from 'axios';
+
+// const token = localStorage.getItem('token');
+
+// const header = {
+//     headers: {
+//         Authorization: `Bearer ${token}`,
+//         'Access-Control-Allow-Origin': '*',
+//         'Content-Type': 'application/json',
+//     },
+// };
+
+// // todo 작성.
+// const addTodo = async (payload) => {
+//     return await axios.post(`${process.env.REACT_APP_API_URL}todos`, payload, header);
+// };
+
+// // todo 조회.
+// const getTodo = async () => {
+//     return await axios.get(`${process.env.REACT_APP_API_URL}todos`, header);
+// };
+
+// // todo 수정.
+// const updateTodo = async (id, payload) => {
+//     return await axios.put(`${process.env.REACT_APP_API_URL}todos/${id}`, payload, header);
+// };
+
+// // todo 삭제.
+// const deleteTodo = async (id) => {
+//     return await axios.delete(`${process.env.REACT_APP_API_URL}todos/${id}`, header);
+// };
+
 
 export { addTodo, getTodo, updateTodo, deleteTodo };
--- src/api/apiUser.js
+++ src/api/apiUser.js
@@ -1,19 +1,37 @@
-import axios from 'axios';
-
-const header = {
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
+import { apiClient } from './apiClient';
 
 // 회원가입.
 const userSignUp = async (payload) => {
-    return await axios.post(`${process.env.REACT_APP_API_URL}auth/signup`, payload, header);
+    return await apiClient.post('/auth/signup', payload);
 };
 
 // 로그인.
 const userLogin = async (payload) => {
-    return await axios.post(`${process.env.REACT_APP_API_URL}auth/signin`, payload, header);
+    return await apiClient.post('/auth/signin', payload);
 };
+
+// 이전 버전 코드.
+// 참고용도로 삭제하지는 않고 전체 주석처리.
+// 이전 버전의 경우 토큰이 웹 스토리지에 존재함에도, 이를 제대로 가져오기 전에 서버에 요청을 먼저 보내어..
+// 토큰이 없는 상황에서 서버에 인증을 요청하여 지속적으로 문제를 발생시켰음.
+
+// import axios from 'axios';
+
+// const header = {
+//     headers: {
+//         'Access-Control-Allow-Origin': '*',
+//         'Content-Type': 'application/json',
+//     },
+// };
+
+// // 회원가입.
+// const userSignUp = async (payload) => {
+//     return await axios.post(`${process.env.REACT_APP_API_URL}auth/signup`, payload, header);
+// };
+
+// // 로그인.
+// const userLogin = async (payload) => {
+//     return await axios.post(`${process.env.REACT_APP_API_URL}auth/signin`, payload, header);
+// };
 
 export { userSignUp, userLogin };
